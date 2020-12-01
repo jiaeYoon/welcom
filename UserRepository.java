89274ddea1878dfe6039a6fa2c61ae8336031472
jayoon <yoon529@sookmyung.ac.kr>
2020-12-01 19:56:52 +0900

    test6

--- UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ja.jademo.repository;
-
-import com.ja.jademo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
