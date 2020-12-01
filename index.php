f2ed5966ff9930ce8c835e17f73c5412d7254468
jayoon <yoon529@sookmyung.ac.kr>
2020-12-01 17:14:19 +0900

    php for test

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+t<!doctype html>
 
 <html xmlns:th="http://www.thymeleaf.org">
 <head th:replace="fragments/common :: head('JA KOREA')">
@@ -18,7 +18,27 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">회의실 예약</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">차량관리</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">교재재고 관리</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">선수금</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="1index.php">교육실적 관리</a>
+        </li>
+    </div> 
 </nav>
 </header>
 
